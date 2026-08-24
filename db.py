dc01966191fad62be91b5d56eb344febf6fca983
Raghavvats28 <rvats2006@gmail.com>
2026-08-24 16:32:48 +0530

    Fix import statement for config module

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-from .config import get_settings
+from config import get_settings
 
 settings = get_settings()
 connect_args = {"check_same_thread": False} if settings.database_url.startswith("sqlite") else {}
